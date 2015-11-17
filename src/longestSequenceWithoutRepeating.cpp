4bdbddb1ed47923570a29ec001df39ef43d2c3b8
theneuronarc <theneuronarc@gmail.com>
2015-11-17 13:09:49 +0530

    Update longestSequenceWithoutRepeating.cpp
    Added description for longestStringWithoutRepeating

--- src/longestSequenceWithoutRepeating.cpp
+++ src/longestSequenceWithoutRepeating.cpp
@@ -1,4 +1,13 @@
+/* 
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string, find the length of the longest substring without repeating characters. For example, 
+the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
+For "bbbbb" the longest substring is "b", with the length of 1.
+	
+Special cases
+"", "aa", "au", "abba", "dvdf", "bbbbb"
+*/
 
 #define max(a,b) (a>b?a:b)
 
