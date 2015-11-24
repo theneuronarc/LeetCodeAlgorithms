e408c26383353df342fc80067e0600403376d172
theneuronarc <theneuronarc@gmail.com>
2015-11-24 17:04:35 +0530

    Resolved compilation error

--- src/remove_element.cpp
+++ src/remove_element.cpp
@@ -4,6 +4,9 @@ https://leetcode.com/problems/remove-element/
 Given an array and a value, remove all instances of that value in place and return the new length.
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 */
+
+#include "std_headers.h"
+
 class Solution {
 public:
 	int removeDuplicates(vector<int>& nums, int value) {
