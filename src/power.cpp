d9a861ff1e2797dc9083bbaf58cdcdaa5b6b0781
theneuronarc <theneuronarc@gmail.com>
2015-12-03 06:58:21 +0530

    [Accepted] Power

--- src/power.cpp
+++ src/power.cpp
@@ -4,10 +4,12 @@ https://leetcode.com/problems/powx-n/
 Implement pow(x, n).
 */
 
+#include "std_headers.h"
+
 class Power {
 public:
-	double table[32] = { 0.0 };
-	bool tableMap[32] = { false };
+	double* table;
+	bool* tableMap;
 
 	double myPowRecurse(double x, int n) {
 		double result = 1.0;
@@ -26,14 +28,50 @@ public:
 	}
  
 	double myPow(double x, int n) {
+		bool sign = (n < 0);
+		double result;
+
+		n = abs(n);
+
+		if (x == 0.0 || x == 1.0)
+			return x;
+		
+		table = new double[n+1]();
+		tableMap = new bool[n+1]();
 		table[0] = 1.0;
 		table[1] = x;
-		tableMap[0] = table[1] = true;
-		return myPowRecurse(x, n);
+		tableMap[0] = tableMap[1] = true;
+		result =  myPowRecurse(x, n);
+		if (sign)
+			return (1 / result);
+		else
+			return result;
 	}
 };
 
+
+class Solution {
+public:
+	double myPow(double x, int n) {
+		bool sign = (n < 0);
+		double result;
+
+		n = abs(n);
+
+		if (x == 0.0 || x == 1.0)
+			return x;
+
+
+
+		if (sign)
+			return (1 / result);
+		else
+			return result;
+	}
+};
+
+
 void powerMain() {
 	Power obj;
-	obj.myPow(8.88023, 3);
+	cout << obj.myPow(0.00001, 2147483647);
 }
