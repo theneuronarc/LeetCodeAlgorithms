986e3f110ba4b33603003daa962ed073600e5071
theneuronarc <theneuronarc@gmail.com>
2015-12-09 17:04:32 +0530

    [Incomplete] Valid Numbers

---
+++ src/valid_number.cpp
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/valid-number/
+
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous.
+You should gather all requirements up front before implementing one.
+*/
+
+#include "std_headers.h"
+
+class Solution {
+public:
+	bool isNumber(string s) {
+
+	}
+};
+
+void validNumbersMain() {
+}
