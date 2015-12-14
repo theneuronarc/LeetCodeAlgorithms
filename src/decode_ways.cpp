c9bbabc53cfb5726b69a60ee5d0aaade6f7eaf74
theneuronarc <theneuronarc@gmail.com>
2015-12-14 18:59:48 +0530

    [Accepted] Decode ways

--- src/decode_ways.cpp
+++ src/decode_ways.cpp
@@ -4,7 +4,7 @@ https://leetcode.com/problems/decode-ways/
 
 #include "std_headers.h"
 
-class Solution {
+class SolutionListAll {
 public:
 	void combinations(vector<int>& s, int startIdx, vector<int>& subset, vector<vector<int>>& res, int& count) {
 		if (startIdx >= s.size()) {
@@ -43,5 +43,42 @@ public:
 		}
 		combinations(input, 0, subset, res, count);
 		return count;
+	}
+};
+
+class SolutionDP {
+public:
+	int numDecodings(string s) {
+		int count = 0;
+		int lastCount[2] = { 1, 1 };
+		int num = 0;
+
+		if (s.empty() || s[0] == '0')
+			return 0;
+
+
+		for (int i = 1; i < s.size(); i++) {
+			if (s[i] != '0')
+				count = lastCount[1];
+			else
+				count = 0;
+
+			if (s[i - 1] != '0') {
+				num = (s[i - 1] - '0') * 10 + (s[i] - '0');
+				if (num <= 26) {
+					count += lastCount[0];
+				}
+			}
+			else
+				count += 0;
+
+			if (!count)
+				return 0;
+
+			lastCount[0] = lastCount[1];
+			lastCount[1] = count;
+		}
+
+		return lastCount[1];
 	}
 };
