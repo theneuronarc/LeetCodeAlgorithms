2e2e5b27def01e50a8daea216bd2256eada1e260
theneuronarc <theneuronarc@gmail.com>
2016-03-09 13:16:29 +0530

    Update inorder_postorder.cpp

--- src/inorder_postorder.cpp
+++ src/inorder_postorder.cpp
@@ -1,5 +1,51 @@
 #include "std_headers.h"
 
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int size = inorder.size();
+        
+        TreeNode** path = (TreeNode**)malloc(sizeof(TreeNode*)*(size + 1));
+        TreeNode* dummy = new TreeNode(0xDEADBEEF);
+        path[0] = dummy;
+        TreeNode* root = dummy;
+        
+        int pathIdx = 0;
+        int inIdx = size - 1;
+        int postIdx = size - 1;
+        
+        while(postIdx >= 0){
+            if(path[pathIdx]->val == inorder[inIdx]){
+            	// Check Retreat
+                root = path[pathIdx];
+                pathIdx--;
+                inIdx--;
+            }
+            else{
+            	// Form right chain for left subtree.
+                TreeNode* head= new TreeNode(0);
+                TreeNode* node = head;
+                
+                while(postorder[postIdx] != inorder[inIdx]){
+                    node->right = new TreeNode(postorder[postIdx]);
+                    postIdx--;
+                    node = node->right;
+                    path[++pathIdx] = node;
+                }
+                
+                node->right = new TreeNode(postorder[postIdx]);
+                node = node->right;
+                path[++pathIdx] = node;
+                postIdx--;
+                root->left = head->right;
+                delete head;
+            }
+        }
+        
+        return dummy->left;
+    }
+};
+
 int search(int* data, int size, int val) {
 	for (int i = 0; i < size; i++) {
 		if (data[i] == val)
